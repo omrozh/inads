92b6de26bb3faa0dfe9a9f4c04418931ab9dae3d
omrozh <omrozh@hotmail.com>
2021-02-17 00:03:55 +0300

    new commit

--- ads.js
+++ ads.js
@@ -3,7 +3,7 @@ let adname = ""
 let apiKey = ""
 
 function createAds(element, index){
-  fetch("http://inads.herokuapp.com/view/" + element.getAttribute("name"))
+  fetch("https://inads.herokuapp.com/view/" + element.getAttribute("name"))
   .then(res=>{urlfinal = res.url; return res.blob()})
   .then(blob=>{
     var img = URL.createObjectURL(blob);
@@ -48,5 +48,5 @@ for(var i = 0; i < adElements.length; i++) {
 }
 
 function clickad(index){
-    document.location = "http://inads.herokuapp.com/adclick/" + index
+    document.location = "https://inads.herokuapp.com/adclick/" + index
 }
