792cc86b8fbbafd3998bf01e428a7e39e8c7ec6e
omrozh <omrozh@hotmail.com>
2021-02-17 19:24:28 +0300

    new commit

--- ads.js
+++ ads.js
@@ -8,7 +8,7 @@ function createAds(element, index){
   .then(res=>{urlfinal = res.url; return res.blob()})
   .then(blob=>{
     if(blob.size < 150){
-        alert("no ads")
+        alert("no ads " + blob.size)
         return "Cannot complete action"
     }
     var img = URL.createObjectURL(blob);
