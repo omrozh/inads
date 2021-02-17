4ae9f464c709de9a1f24d240f3e9f86ed71e9d6c
omrozh <omrozh@hotmail.com>
2021-02-17 19:15:23 +0300

    new commit

--- app.py
+++ app.py
@@ -365,7 +365,7 @@ def return_file(adtype):
 
     try:
         suitablead = suitableads[random.randint(0, len(suitableads) - 1)]
-    except ValueError:
+    except:
         pass
 
     if not suitablead:
