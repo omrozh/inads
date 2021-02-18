5f1e51354dd5b2e711b5839567be92792700d4ab
omrozh <omrozh@hotmail.com>
2021-02-18 19:28:44 +0300

    new commit

--- ads.js
+++ ads.js
@@ -13,8 +13,10 @@ function createAds(element, index){
     }
     var img = URL.createObjectURL(blob);
     element.setAttribute('src', img);
-    if(element.getAttribute("float") != "right" && element.getAttribute("float") != "left")
+    if(element.getAttribute("float") != "right" && element.getAttribute("float") != "left"){
+        alert(element.getAttribute("float"))
         element.insertAdjacentHTML("afterend", '<br><a href="http://inads.herokuapp.com" style="text-decoration: none; margin-left: ' + element.getAttribute("margin-left") + '; margin-right: ' + element.getAttribute("margin-right") + '; color:yellow; ' + element.getAttribute("height") + ';"><small style="margin-left: 12.5%">Ads by <span style="color: black;">InAds</small></span></a>')
+    }
     if(element.getAttribute("name") == "inadstandard"){
         element.style.removeProperty("width")
         element.style.removeProperty("height")
