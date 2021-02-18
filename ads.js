44b8941b4bca0572a0069f340bd84cae774419cc
omrozh <omrozh@hotmail.com>
2021-02-18 19:41:46 +0300

    new commit

--- ads.js
+++ ads.js
@@ -13,7 +13,7 @@ function createAds(element, index){
     }
     var img = URL.createObjectURL(blob);
     element.setAttribute('src', img);
-    element.insertAdjacentHTML("afterend", '<br><a href="http://inads.herokuapp.com" style="text-decoration: none; margin-left: ' + element.getAttribute("margin-left") + '; margin-right: ' + element.getAttribute("margin-right") + '; color:yellow; ' + element.getAttribute("height") + '; float: ' + window.getComputedStyle(element).float + '"><small style="margin-left: 12.5%">Ads by <span style="color: black;">InAds</small></span></a>')
+    element.insertAdjacentHTML("afterend", '<br><a href="http://inads.herokuapp.com" style="text-decoration: none; margin-left: ' + element.getAttribute("margin-left") + '; margin-right: ' + element.getAttribute("margin-right") + '; color:yellow; position: relative;' + element.getAttribute("height") + '; float: ' + window.getComputedStyle(element).float + '"><small style="margin-left: 12.5%">Ads by <span style="color: black;">InAds</small></span></a>')
 
     if(element.getAttribute("name") == "inadstandard"){
         element.style.removeProperty("width")
