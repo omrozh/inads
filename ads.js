9029f140e7e0c81f71de30867ce58ee637499712
omrozh <omrozh@hotmail.com>
2021-02-18 09:38:02 +0300

    new commit

--- ads.js
+++ ads.js
@@ -51,10 +51,10 @@ document.body.insertAdjacentHTML("beforebegin", "<small> Ads by InAds </small>")
 
 if(adElements.length == 0){
     var insertbanner = '<img class="inads" name=inadsbanner src="" alt="" style="float: right;">';
-    var insertad = '<img class="inads" name=inadstandard src="" alt="" style="margin-left: 9%">';
+    var insertad = '<img class="inads" name=inadstandard src="" alt="" style="margin-left: 12.5%">';
     document.body.insertAdjacentHTML("beforeEnd", insertad);
-    document.body.insertAdjacentHTML("afterbegin", insertad);
     document.body.insertAdjacentHTML("afterbegin", insertbanner);
+    document.body.insertAdjacentHTML("afterbegin", insertad);
     adElements = document.getElementsByClassName("inads");
 }
 
