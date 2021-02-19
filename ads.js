3595e7e8292188fba12d21f461625f650fa782c5
omrozh <omrozh@hotmail.com>
2021-02-19 22:33:47 +0300

    new commit

--- ads.js
+++ ads.js
@@ -13,7 +13,7 @@ function createAds(element, index){
     }
     var img = URL.createObjectURL(blob);
     element.setAttribute('src', img);
-    element.insertAdjacentHTML("beforebegin", '<a href="http://inads.herokuapp.com" style="text-decoration: none; color:yellow; float: ' + window.getComputedStyle(element).float + '"><small style="font-size: 6px; margin-left: ' + window.getComputedStyle(element).marginLeft + '; margin-right: ' + window.getComputedStyle(element).marginRight + '; position:' + window.getComputedStyle(element).position + '; right:' + window.getComputedStyle(element).right + '; left:' + window.getComputedStyle(element).left + '">Ads by <span style="color: black;">InAds</small></span></a><br>')
+    element.insertAdjacentHTML("beforebegin", '<a href="http://inads.herokuapp.com" style="text-decoration: none; color:yellow; float: ' + window.getComputedStyle(element).float + '"><small style="font-size: 6px; margin-left: ' + window.getComputedStyle(element).marginLeft + '; margin-right: ' + window.getComputedStyle(element).marginRight + '">Ads by <span style="color: black;">InAds</small></span></a><br>')
 
     if(element.getAttribute("name") == "inadstandard"){
         element.style.removeProperty("width")
@@ -50,7 +50,7 @@ function createAds(element, index){
 let adElements = document.getElementsByClassName("inads");
 
 if(adElements.length == 0){
-    var insertbanner = '<img class="inads" name=inadsbanner src="" alt="" style="right: 0; position: fixed">';
+    var insertbanner = '<img class="inads" name=inadsbanner src="" alt="" style="float: right; position: fixed; right: 0">';
     var insertad = '<img class="inads" name=inadstandard src="" alt="" style="margin-left: 12.5%;">';
     document.body.insertAdjacentHTML("beforeEnd", insertad);
     document.body.insertAdjacentHTML("afterbegin", insertbanner);
