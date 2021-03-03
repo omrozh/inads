15ced03e07565e0de5b0d2f21adaaff608e822a1
omrozh <omrozh@hotmail.com>
2021-03-03 08:15:15 +0300

    hh

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def addDomain():
             db.session.commit()
             return flask.redirect("/dashboard")
         try:
-            requestinfo = b'info@tatilkupu.com'
+            requestinfo = requests.get("http://" + flask.request.values["domain"] + "/inadsconfirm.txt").content
             url = "http://" + flask.request.values["domain"]
 
             requestobject = requests.get(url).content.decode("utf-8")
