7b5f2d50368285b03a015ba4a00b57033ff6c275
omrozh <omrozh@hotmail.com>
2021-03-09 20:09:08 +0300

    hh

--- ads.js
+++ ads.js
@@ -8,7 +8,7 @@ function createAds(element, index){
   .then(res=>{adname = res.url.substring(res.url.lastIndexOf("/") + 1); element.setAttribute('onclick', "c(" + adname + ")"); return res.blob()})
   .then(blob=>{
 
-    if(blob.size < 500){
+    if(blob.size < 150){
         return "Cannot complete action"
     }
     var img = URL.createObjectURL(blob);
