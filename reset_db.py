de4795bfe0f03e0c3b8bd82504374f650a18cdbe
omrozh <omrozh@hotmail.com>
2021-03-18 20:13:51 +0300

    hh

---
+++ reset_db.py
@@ -0,0 +1,18 @@
+from app import db, User, Ads, Domains
+
+user = User.query.all()
+ads = Ads.query.all()
+domains = Domains.query.all()
+
+db.drop_all()
+db.create_all()
+
+
+for i in user:
+    db.session.add(i)
+
+for i in ads:
+    db.session.add(i)
+
+for i in domains:
+    db.session.add(i)
