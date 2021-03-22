9568f52709568eceddd09e8c882f5e266b01728f
omrozh <omrozh@hotmail.com>
2021-03-22 10:23:11 +0300

    hhh

--- app.py
+++ app.py
@@ -141,12 +141,10 @@ def makePayment(credit, month, year, cvc, create_subscription):
 
 @app.before_request
 def before_request():
-    if flask.request.is_secure:
-        return
-
-    url = flask.request.url.replace("http://", "https://", 1)
-    code = 301
-    return flask.redirect(url, code=code)
+    if not flask.request.is_secure:
+        url = flask.request.url.replace("http://", "https://", 1)
+        code = 301
+        return flask.redirect(url, code=code)
 
 
 @app.route("/status")
