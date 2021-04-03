5c29ed48640dc0868188fcd65ef6b92b22f1e1a0
omrozh <omrozh@hotmail.com>
2021-04-03 16:33:07 +0300

    hhh

--- app.py
+++ app.py
@@ -756,7 +756,7 @@ def return_file(adtype, pagetitleinc):
         domainList.append(str(i.domain))
 
     if domain not in domainList:
-        return "Unauthorized request"
+        return "Unauthorized request " + domain + domainList
 
     all_paused_ads = []
 
