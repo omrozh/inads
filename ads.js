092d311627e1f57f1ee4db5eb12c1f869be78246
omrozh <omrozh@hotmail.com>
2021-04-05 08:58:26 +0300

    hhh

--- ads.js
+++ ads.js
@@ -27,7 +27,8 @@ function createAds(element, index){
 
     var img = blob;
     element.setAttribute('src', img);
-    element.insertAdjacentHTML("beforebegin", '<a href="http://www.inadsglobal.com" style="text-decoration: none; color:yellow; float: ' + window.getComputedStyle(element).float + '"><small style="font-size: 6px; margin-left: ' + window.getComputedStyle(element).marginLeft + '; margin-right: ' + window.getComputedStyle(element).marginRight + '">Ads by <span style="color: black;">InAds</small></span></a><br>')
+    if(element.getAttribute("name") != "inadsquare")
+        element.insertAdjacentHTML("beforebegin", '<a href="http://www.inadsglobal.com" style="text-decoration: none; color:yellow; float: ' + window.getComputedStyle(element).float + '"><small style="font-size: 6px; margin-left: ' + window.getComputedStyle(element).marginLeft + '; margin-right: ' + window.getComputedStyle(element).marginRight + '">Ads by <span style="color: black;">InAds</small></span></a><br>')
 
     if(element.getAttribute("name") == "inadstandard"){
         element.style.removeProperty("width")
