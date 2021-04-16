ee646ba7b779d080d357b795df2dce95bf0c9ffb
omrozh <omrozh@hotmail.com>
2021-04-16 11:34:27 +0300

    h

--- ads.js
+++ ads.js
@@ -27,6 +27,10 @@ function createAds(element, index){
 
     var img = blob;
     element.setAttribute('src', img);
+
+    if(element.getAttribute("name") == "inadsvideo")
+        element.setAttribute("controls","controls");
+
     if(!(element.parentNode.className == "inadsgroup" || element.parentNode.className == "inadsgroupsquare"))
         element.insertAdjacentHTML("beforebegin", '<a href="http://www.inadsglobal.com" style="text-decoration: none; color:yellow; float: ' + window.getComputedStyle(element).float + '"><small style="font-size: 6px; margin-left: ' + window.getComputedStyle(element).marginLeft + '; margin-right: ' + window.getComputedStyle(element).marginRight + '">Ads by <span style="color: black;">InAds</small></span></a>')
 
