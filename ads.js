1b52dc9e92793ece1faed18650c4afc60062c730
omrozh <omrozh@hotmail.com>
2021-04-16 12:06:27 +0300

    h

--- ads.js
+++ ads.js
@@ -30,6 +30,11 @@ function createAds(element, index){
 
     if(element.getAttribute("name") == "inadsvideo")
         element.play()
+        var store = element.onclick;
+        element.onclick = ""
+        if(element.paused){
+            element.onclick = "element.onclick = " + store
+        }
         element.onplay = element.muted = !element.muted;
         element.volume = 0.4
         element.setAttribute("controls","controls");
