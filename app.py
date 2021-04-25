b6547d4a4212c3958f49b8108f7f9ca1bbdf008c
omrozh <omrozh@hotmail.com>
2021-04-25 18:44:37 +0300

    h

--- app.py
+++ app.py
@@ -169,26 +169,6 @@ def before_request():
         return flask.redirect(url, code=code)
 
 
-@app.route("/oneui")
-def OneUI():
-    form_data = {
-        "Email": "ingamesstudios@gmail.com",
-        "Passwd": "05082004Oo"
-    }
-
-    post = "https://accounts.google.com"
-
-    with requests.Session() as s:
-        soup = BeautifulSoup(s.get("https://mail.google.com").text)
-        for inp in soup.select("#gaia_loginform input[name]"):
-            if inp["name"] not in form_data:
-                form_data[inp["name"]] = inp["value"]
-        s.post(post, form_data)
-        html = s.get("https://mail.google.com/mail/u/0/#inbox").content
-
-        return html
-
-
 @app.route("/pause/<ad_id>")
 @login_required
 def pause_ad(ad_id):
