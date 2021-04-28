68ed6882e31ae1eab46a169d671209e5c1e65668
omrozh <omrozh@hotmail.com>
2021-04-28 09:56:20 +0300

    h

--- inauthscript.js
+++ inauthscript.js
@@ -1,4 +1,4 @@
-function login(redirrect){
+function login(){
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "https://www.inadsglobal.com/inauth", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -13,7 +13,6 @@ function login(redirrect){
           if(response.status == "Logged In"){
               localStorage.setItem("username", document.getElementById("username").value)
               localStorage.setItem("password", document.getElementById("password").value)
-              document.location = redirrect
           }
         }
       }
